31b0b18df5d00bdc61b14306c291e344c2e0a242
diesisfox <james.zhi.liu@gmail.com>
2017-05-04 22:47:56 -0400

    Imported and updated most files for IAR

--- Src/main.c
+++ Src/main.c
@@ -4,37 +4,37 @@
   * Description        : Main program body
   ******************************************************************************
   *
-  * Copyright (c) 2017 STMicroelectronics International N.V. 
+  * Copyright (c) 2017 STMicroelectronics International N.V.
   * All rights reserved.
   *
-  * Redistribution and use in source and binary forms, with or without 
+  * Redistribution and use in source and binary forms, with or without
   * modification, are permitted, provided that the following conditions are met:
   *
-  * 1. Redistribution of source code must retain the above copyright notice, 
+  * 1. Redistribution of source code must retain the above copyright notice,
   *    this list of conditions and the following disclaimer.
   * 2. Redistributions in binary form must reproduce the above copyright notice,
   *    this list of conditions and the following disclaimer in the documentation
   *    and/or other materials provided with the distribution.
-  * 3. Neither the name of STMicroelectronics nor the names of other 
-  *    contributors to this software may be used to endorse or promote products 
+  * 3. Neither the name of STMicroelectronics nor the names of other
+  *    contributors to this software may be used to endorse or promote products
   *    derived from this software without specific written permission.
-  * 4. This software, including modifications and/or derivative works of this 
+  * 4. This software, including modifications and/or derivative works of this
   *    software, must execute solely and exclusively on microcontroller or
   *    microprocessor devices manufactured by or for STMicroelectronics.
-  * 5. Redistribution and use of this software other than as permitted under 
-  *    this license is void and will automatically terminate your rights under 
-  *    this license. 
+  * 5. Redistribution and use of this software other than as permitted under
+  *    this license is void and will automatically terminate your rights under
+  *    this license.
   *
-  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
-  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
-  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
   * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
-  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT
   * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
   * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
   * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
   * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
@@ -46,6 +46,18 @@
 #include "cmsis_os.h"
 
 /* USER CODE BEGIN Includes */
+#include "can.h"
+#include "serial.h"
+#include "nodeMiscHelpers.h"
+#include "nodeConf.h"
+#include "../../CAN_ID.h"
+
+// RTOS Task functions + helpers
+#include "Can_Processor.h"
+#ifndef DISABLE_RT
+//#include "RT_Handler.h"
+#endif
+#include "Node_Manager.h"
 
 /* USER CODE END Includes */
 
@@ -71,7 +83,15 @@ osMutexId controlVarsMtxHandle;
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
+#ifndef NODE_CONFIGURED
+#error "NODE NOT CONFIGURED. GO CONFIGURE IT IN NODECONF.H!"
+#endif
 
+//osMutexId nodeEntryMtxHandle[16];		// Mutex for every node table entry
+QueueHandle_t * nodeEntryMtxHandle = (QueueHandle_t[MAX_NODE_NUM]){};
+osTimerId * nodeTmrHandle = (osTimerId[MAX_NODE_NUM]){};			// Timer for each node's timeout timer
+nodeEntry * nodeTable = (nodeEntry[MAX_NODE_NUM]){};
+controlVars userInput;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -93,14 +113,28 @@ void doNodeManager(void const * argument);
 /* USER CODE END PFP */
 
 /* USER CODE BEGIN 0 */
+// Handler for node HB timeout
+void TmrHBTimeout(void const * argument){
+	// TODO: Test if using point in the line below breaks this function
+ 	uint8_t timerID = (uint8_t)pvTimerGetTimerID((TimerHandle_t)argument);
+#ifdef DEBUG
+ 	Serial2_write(timerID); //Found your raw data, Frank. >:|
+ 	static uint8_t msg[] = "HB Timeout\n";
+	Serial2_writeBytes(msg,sizeof(msg)-1);
 
+#endif
+	nodeTable[timerID].nodeConnectionState = UNRELIABLE;
+	if((timerID) != mc_nodeID){
+		xQueueSend(BadNodesQHandle, &timerID, portMAX_DELAY);
+	}
+}
 /* USER CODE END 0 */
 
 int main(void)
 {
 
   /* USER CODE BEGIN 1 */
-
+	setState(ACTIVE);
   /* USER CODE END 1 */
 
   /* MCU Configuration----------------------------------------------------------*/
@@ -119,6 +153,24 @@ int main(void)
   MX_WWDG_Init();
 
   /* USER CODE BEGIN 2 */
+#ifdef DEBUG
+	static uint8_t hbmsg[] = "Command Center booting... \n";
+	Serial2_writeBytes(hbmsg, sizeof(hbmsg)-1);
+#endif
+  setupNodeTable();
+  nodeTable[cc_nodeID].nodeStatusWord = ACTIVE;		// Set initial status to ACTIVE
+
+  ////*IF YOU GET HCAN1 NOT DEFINED ERROR, CHECK NODECONF.H FIRST!*////
+  bxCan_begin(&hcan1, &mainCanRxQHandle, &mainCanTxQHandle);
+  // Set up CAN filter banks
+  //XXX forgot the P2P commands?
+  bxCan_addMaskedFilterStd(swOffset,0xFF0,0); // Filter: Status word group (ignore nodeID)
+  bxCan_addMaskedFilterStd(fwOffset,0xFF0,0); // Filter: Firmware version group (ignore nodeID)
+  bxCan_addMaskedFilterStd(p2pOffset,0xFF0,0); // Filter: p2p command group (ignore nodeID)
+
+  bxCan_addMaskedFilterExt(mitsubaFr0,0xFFFFFF0F,0);	// Mitsuba Frame 0
+  bxCan_addMaskedFilterExt(mitsubaFr1,0xFFFFFF0F,0);	// Mitsuba Frame 1
+  bxCan_addMaskedFilterExt(mitsubaFr2,0xFFFFFF0F,0);	// Mitsuba Frame 2
 
   /* USER CODE END 2 */
 
@@ -129,6 +181,12 @@ int main(void)
 
   /* USER CODE BEGIN RTOS_MUTEX */
   /* add mutexes, ... */
+  // Node table entry mutex
+  // Every entry has a mutex that is associated with the nodeID
+  for(uint8_t i =0; i < MAX_NODE_NUM; i++){
+	  osMutexDef(i);
+	  nodeEntryMtxHandle[i] = (QueueHandle_t)osMutexCreate(osMutex(i));
+  }
   /* USER CODE END RTOS_MUTEX */
 
   /* USER CODE BEGIN RTOS_SEMAPHORES */
@@ -137,12 +195,27 @@ int main(void)
 
   /* USER CODE BEGIN RTOS_TIMERS */
   /* start timers, add new ones, ... */
+  // Node heartbeat timeout timers
+    for(uint8_t TmrID = 0; TmrID < MAX_NODE_NUM; TmrID++){
+  	  osTimerDef(TmrID, TmrHBTimeout);
+  	  // TODO: Consider passing the nodeTmrHandle+Offset or NULL
+  	  nodeTmrHandle[TmrID] = osTimerCreate(osTimer(TmrID), osTimerOnce, TmrID);	// TmrID here is stored directly as a variable
+  	  //DISCUSS changePeriod starts the damn timers...
+  	  xTimerChangePeriod(nodeTmrHandle[TmrID], Node_HB_Interval, portMAX_DELAY);
+  	  xTimerStop(nodeTmrHandle[TmrID], portMAX_DELAY);
+  	  //TODO investigate the timer crashes
+  	  // One-shot timer since it should be refreshed by the Can Processor upon node HB reception
+    }
   /* USER CODE END RTOS_TIMERS */
 
   /* Create the thread(s) */
   /* definition and creation of Kick_Dog */
   osThreadDef(Kick_Dog, doKickDOg, osPriorityRealtime, 0, 256);
   Kick_DogHandle = osThreadCreate(osThread(Kick_Dog), NULL);
+
+  /* definition and creation of RT_Handler */
+  osThreadDef(RT_Handler, doRealTime, osPriorityAboveNormal, 0, 512);
+  RT_HandlerHandle = osThreadCreate(osThread(RT_Handler), NULL);
 
   /* definition and creation of Can_Processor */
   osThreadDef(Can_Processor, doProcessCan, osPriorityNormal, 0, 512);
@@ -184,11 +257,11 @@ int main(void)
   /* USER CODE BEGIN RTOS_QUEUES */
   /* add queues, ... */
   /* USER CODE END RTOS_QUEUES */
- 
+
 
   /* Start scheduler */
   osKernelStart();
-  
+
   /* We should never get here as control is now taken by the scheduler */
 
   /* Infinite loop */
@@ -212,13 +285,13 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct;
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
 
-    /**Configure the main internal regulator output voltage 
+    /**Configure the main internal regulator output voltage
     */
   __HAL_RCC_PWR_CLK_ENABLE();
 
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-    /**Initializes the CPU, AHB and APB busses clocks 
+    /**Initializes the CPU, AHB and APB busses clocks
     */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
@@ -235,7 +308,7 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 
-    /**Initializes the CPU, AHB and APB busses clocks 
+    /**Initializes the CPU, AHB and APB busses clocks
     */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
@@ -249,11 +322,11 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 
-    /**Configure the Systick interrupt time 
+    /**Configure the Systick interrupt time
     */
   HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
-    /**Configure the Systick 
+    /**Configure the Systick
     */
   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
@@ -319,10 +392,10 @@ static void MX_WWDG_Init(void)
 
 }
 
-/** 
+/**
   * Enable DMA controller clock
   */
-static void MX_DMA_Init(void) 
+static void MX_DMA_Init(void)
 {
   /* DMA controller clock enable */
   __HAL_RCC_DMA1_CLK_ENABLE();
@@ -337,13 +410,13 @@ static void MX_DMA_Init(void)
 
 }
 
-/** Configure pins as 
-        * Analog 
-        * Input 
+/** Configure pins as
+        * Analog
+        * Input
         * Output
         * EVENT_OUT
         * EXTI
-        * Free pins are configured automatically as Analog (this feature is enabled through 
+        * Free pins are configured automatically as Analog (this feature is enabled through
         * the Code Generation settings)
 */
 static void MX_GPIO_Init(void)
@@ -367,23 +440,23 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PC0 PC1 PC2 PC3 
-                           PC4 PC5 PC6 PC7 
-                           PC8 PC9 PC10 PC11 
+  /*Configure GPIO pins : PC0 PC1 PC2 PC3
+                           PC4 PC5 PC6 PC7
+                           PC8 PC9 PC10 PC11
                            PC12 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3 
-                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7 
-                          |GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11 
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7
+                          |GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11
                           |GPIO_PIN_12;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PA0 PA1 PA4 PA6 
-                           PA7 PA8 PA9 PA10 
+  /*Configure GPIO pins : PA0 PA1 PA4 PA6
+                           PA7 PA8 PA9 PA10
                            PA15 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_4|GPIO_PIN_6 
-                          |GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10 
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_4|GPIO_PIN_6
+                          |GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10
                           |GPIO_PIN_15;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
@@ -396,13 +469,13 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PB0 PB1 PB2 PB10 
-                           PB12 PB13 PB14 PB15 
-                           PB4 PB5 PB6 PB7 
+  /*Configure GPIO pins : PB0 PB1 PB2 PB10
+                           PB12 PB13 PB14 PB15
+                           PB4 PB5 PB6 PB7
                            PB8 PB9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_10 
-                          |GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15 
-                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7 
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_10
+                          |GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7
                           |GPIO_PIN_8|GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
@@ -429,12 +502,18 @@ void doKickDOg(void const * argument)
 {
 
   /* USER CODE BEGIN 5 */
+#ifndef DISABLE_RT
   /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
+  uint32_t PreviousWakeTime = osKernelSysTick();
+  uint8_t motorCanErrCount = 0;
+  // Wrapper for RT_Handler task
+//  RT_Handler(&PreviousWakeTime, &motorCanErrCount);
+#else
+  for(;;){
+	  osDelay(10000);
   }
-  /* USER CODE END 5 */ 
+#endif
+  /* USER CODE END 5 */
 }
 
 /* doProcessCan function */
@@ -442,10 +521,9 @@ void doProcessCan(void const * argument)
 {
   /* USER CODE BEGIN doProcessCan */
   /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
+	// Wrapper function for the CAN Processing Logic
+	// Handles all CAN Protocol Suite based responses and tasks
+	Can_Processor();
   /* USER CODE END doProcessCan */
 }
 
@@ -468,7 +546,8 @@ void doNodeManager(void const * argument)
   /* Infinite loop */
   for(;;)
   {
-    osDelay(1);
+	// Wrapper for the Node_Manager task
+    Node_Manager();
   }
   /* USER CODE END doNodeManager */
 }
@@ -503,10 +582,10 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler */
   /* User can add his own implementation to report the HAL error return state */
-  while(1) 
+  while(1)
   {
   }
-  /* USER CODE END Error_Handler */ 
+  /* USER CODE END Error_Handler */
 }
 
 #ifdef USE_FULL_ASSERT
@@ -531,10 +610,10 @@ void assert_failed(uint8_t* file, uint32_t line)
 
 /**
   * @}
-  */ 
+  */
 
 /**
   * @}
-*/ 
+*/
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
